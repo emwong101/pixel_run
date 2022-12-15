a1f92819804391357d973f1176dc4eb050345140
emwong101 <emilywonglfa@gmail.com>
2022-12-14 23:03:33 -0800

    added caution symbol to about me section

--- src/pages/infoPage/InfoPage.tsx
+++ src/pages/infoPage/InfoPage.tsx
@@ -39,22 +39,36 @@ function InfoPage() {
           <p className="bar__label">Pixel Run</p>
           <input type="button" value="X" className="close"></input>
         </div>
-        <h2 className="about">About Me</h2>
-        <h4 className="about__body">
-          I am a former legal assistant and policy researcher. After spending a
-          little over a year within the legal industry, I decided I needed
-          something more fast paced and modern. Web development highly appealed
-          to me due to the industry's focus on accessible design/functionality,
-          on top of constant innovation. I chose to design a game for my
-          capstone as a way of exploring different areas of the tech industry,
-          and to challenge my own proficiency in Javascript. Ironically, I ended
-          up learning Typescript in order to build my project as TS works better
-          with Phaser's class based nature. Before formal deployment, I hope to
-          add multiplayer functionality and improve upon the object generation
-          algorithim within the game. All the assets used within the game are
-          either open sourced artwork (which can be found
-          <a href="https://opengameart.org/"> here</a>) or original designs.
-        </h4>
+        <div className="about__wrapper">
+          <div className="about__symbol">
+            <img
+              className="caution"
+              alt="caution symbol"
+              src="assets/caution.png"
+            ></img>
+          </div>
+          <div className="about__text-div">
+            <h2 className="about">About Me</h2>
+            <h5 className="about__body">
+              I am a former legal assistant and policy researcher. After
+              spending a little over a year within the legal industry, I decided
+              I needed something more fast paced and modern. Web development
+              highly appealed to me due to the industry's focus on accessible
+              design/functionality, on top of constant innovation.
+            </h5>
+            <h5 className="about__body">
+              I chose to design a game for my capstone as a way of exploring
+              different areas of the tech industry, and to challenge my own
+              proficiency in Javascript. Ironically, I ended up learning
+              Typescript in order to build my project as TS works better with
+              Phaser's class based nature. Before formal deployment, I hope to
+              add multiplayer functionality and improve upon the object
+              generation algorithim within the game. All the assets used within
+              the game are either open sourced artwork (which can be found
+              <a href="https://opengameart.org/"> here</a>) or original designs.
+            </h5>
+          </div>
+        </div>
       </motion.div>
       <motion.div
         className="main third"
